9ed343c55b3fb7d9bf1fac2c3eea02ac4127865f
ShreyaDeveloper2001 <134867803+ShreyaDeveloper2001@users.noreply.github.com>
2023-10-07 19:58:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const buttonText = button.innerText;
+
+        // Handle button click
+        if (buttonText === '=') {
+            // Implement the calculation logic here
+            try {
+                display.value = eval(display.value);
+            } catch (error) {
+                display.value = 'Error';
+            }
+        } else if (buttonText === 'C') {
+            // Clear the display
+            display.value = '';
+        } else if (buttonText === 'Back') {
+            // Remove the last character
+            display.value = display.value.slice(0, -1);
+        } else {
+            // Append the clicked button's text to the display
+            display.value += buttonText;
+        }
+    });
+});
+
